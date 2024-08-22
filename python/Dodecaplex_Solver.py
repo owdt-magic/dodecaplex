57c8825b917ea2115fcbd435c7b64f47ed790285
Owen Thompson <oxt3479@rit.edu>
2024-08-21 18:24:27 -0700

    Working on Dodecaplex generations

--- python/Dodecaplex_Solver.py
+++ python/Dodecaplex_Solver.py
@@ -3,111 +3,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d as a3
 import matplotlib.colors as colors
-from sympy.utilities.iterables import multiset_permutations
-
-PHI = (1+(5**0.5))/2
-EDGE = 3 - (5**0.5)
-
-def calculate_parity(permutation):
-    inversions = 0
-    n = len(permutation)
-    for i in range(n):
-        for j in range(i + 1, n):
-            if permutation[i] > permutation[j]:
-                inversions += 1
-    return inversions % 2 == 0
-
-def unsigned_permutations(a,b,c,d, even=False):
-    all_permutations = []
-    for x in range(2**4):
-        binary = bin(x)
-        binary = '0000'+binary.replace('b', '')
-        
-        sign_permutation = [a*(-1)**(int(binary[-1])+1), b*(-1)**(int(binary[-2])+1), 
-                            c*(-1)**(int(binary[-3])+1), d*(-1)**(int(binary[-4])+1)]
-        these_permutations = []
-        for index_perm in multiset_permutations(list(range(4))):
-            if calculate_parity(index_perm) or not even:
-                these_permutations.append(tuple(sign_permutation[i] for i in index_perm))
-        all_permutations.extend(set(these_permutations))
-    return {*all_permutations}
-
-def test_initialization():
-    assert len(unsigned_permutations(0,       0,              2,          2           )) == 24
-    assert len(unsigned_permutations(PHI,     PHI,            PHI,        (PHI**(-2)) )) == 64
-    assert len(unsigned_permutations(1,       1,              1,          (5**(0.5))  )) == 64
-    assert len(unsigned_permutations((1/PHI), (1/PHI),        (1/PHI),    (PHI*PHI)   )) == 64
-    assert len(unsigned_permutations(0,       (1/PHI),        PHI,        (5**(0.5)), even=True)) == 96
-    assert len(unsigned_permutations(0,       (PHI**(-2)),    1,          (PHI**2),   even=True)) == 96
-    assert len(unsigned_permutations((1/PHI), 1,              PHI,        2,          even=True)) == 192
-
-vertices = tuple( \
-tuple(unsigned_permutations(0,       0,              2,          2           ))+\
-tuple(unsigned_permutations(PHI,     PHI,            PHI,        (PHI**(-2)) )) +\
-tuple(unsigned_permutations(1,       1,              1,          (5**(0.5))  )) +\
-tuple(unsigned_permutations((1/PHI), (1/PHI),        (1/PHI),    (PHI*PHI)   )) +\
-tuple(unsigned_permutations(0,       (1/PHI),        PHI,        (5**(0.5)), even=True)) +\
-tuple(unsigned_permutations(0,       (PHI**(-2)),    1,          (PHI**2),   even=True)) +\
-tuple(unsigned_permutations((1/PHI), 1,              PHI,        2,          even=True)))
-# last two are problmeatic (3 edges)
+from fourdsolids import PHI, EDGE_120CELL, EDGE_600CELL, RADIUS_120CELL, gen_dodecaplex_vertices, gen_tetraplex_vertices
 
 def plot_2d_projection(vertices):
-    colors =  [(len([None for vx in vertices if np.linalg.norm(np.array(v)-np.array(vx)) < .765])**4)/10 \
-                    for v in vertices]
-
+    vertices = np.array(vertices)
     plt.figure(figsize=(8, 8))
-    plt.scatter(vertices[:, 0], vertices[:, 2], c=colors, s=colors)
-    for v, color in zip(vertices, colors):
+    plt.scatter(vertices[:, 0], vertices[:, 2])
+    for v in vertices:
         neighbors = np.array([vx-v for vx in vertices if np.linalg.norm(np.array(v)-np.array(vx)) < .765])
-        if len(neighbors) == 5 : continue
         plt.quiver([v[0] for x in range(len(neighbors))], 
                    [v[1] for y in range(len(neighbors))], neighbors[:,0], neighbors[:,1], 
                    color=['red' for c in range(len(neighbors))], angles='xy', scale_units='xy', scale=1,
                    headwidth=4, headlength=3, width=0.002)
 
     plt.title(f'2D Projection of vertices')
-    plt.grid(True)
-    plt.axis('equal')
     plt.show()
 
-def plot_3d_projection(vertices):
-    scaled_vertices = vertices*(vertices[:,-1]**2)[:, np.newaxis]
+def plot_3d_projection(vertices, EDGE=EDGE_120CELL):
+    vertices = np.array(vertices)
+    #scaled_vertices = vertices/abs(vertices[:,-1])[:, np.newaxis]
+    scaled_vertices = vertices/(PHI**(vertices[:,-1]/10))[:, np.newaxis]
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(projection='3d')
     ax.scatter3D(scaled_vertices[:, 0], scaled_vertices[:, 1], scaled_vertices[:, 2], s=0.1)
-    threes, fours = 0,0
-    for v,vs in zip(vertices, scaled_vertices):
-        neighbors = np.array([sv-vs for sv, vx in zip(scaled_vertices, vertices) if ((EDGE-0.01) < np.linalg.norm(np.array(v)-np.array(vx)) < (EDGE+0.01))]) 
+    for ov,osv in zip(vertices, scaled_vertices):
+        neighbors = np.array([sv-osv for v, sv in zip(vertices, scaled_vertices) if ((EDGE-0.01) < np.linalg.norm(np.array(ov)-np.array(v)) < (EDGE+0.01))]) 
         color_new = random.choice(['red', 'green', 'blue', 'yellow', 'purple', 'cyan'])
-        plt.quiver([vs[0] for x in range(len(neighbors))], 
-                   [vs[1] for y in range(len(neighbors))], 
-                   [vs[2] for z in range(len(neighbors))], neighbors[:,0], neighbors[:,1], neighbors[:,2],
+        plt.quiver([osv[0] for x in range(len(neighbors))], 
+                   [osv[1] for y in range(len(neighbors))], 
+                   [osv[2] for z in range(len(neighbors))], neighbors[:,0], neighbors[:,1], neighbors[:,2],
                    color=[color_new for c in range(len(neighbors))], alpha=0.4)
 
-    print("THREE: ", threes, " FOUR: ", fours)
     plt.title(f'3D Projection of vertices')
     plt.show()
 
-def plot_triangles():
-    """
-    For every point: find it's neighbors. (there will be 48 redundant square neighbor points)
-
-    Create triangles from the neighbors. Skip 7 points - they should be accounted last.
-
-    Determine whether 7 points were safe to ignore (already accounted by preceeding triangles)
-
-    """
+def plot_triangles(vertices, EDGE=EDGE_120CELL):
 
     neighborhoods = {}
 
-    for four_d_point in vertices:    
-        neighborhood = [v for v in vertices if 0.0 < np.linalg.norm(np.array(v)-np.array(four_d_point)) < EDGE+0.01]
-        neighborhoods[four_d_point] = neighborhood
+    for ov in vertices:    
+        neighborhood = [v for v in vertices if EDGE-0.01 < np.linalg.norm(np.array(v)-np.array(ov)) < EDGE+0.01]
+        neighborhoods[ov] = neighborhood
 
     triangles = []
     blacklist= []
-    for origin, neighbors in neighborhoods.items():
-        if origin in blacklist: continue
+    items = list(neighborhoods.items())
+    random.shuffle(items)
+
+    for origin, neighbors in items:
+        #if origin in blacklist: continue
 
         for a,b in ((0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)):
             triangles.append([origin, neighbors[a], neighbors[b]])
@@ -122,11 +66,66 @@ def plot_triangles():
     b = 2
     ax.set(xlim=(-b,b), ylim=(-b,b), zlim=(-b,b))
     fig.add_axes(ax)
-    for x in range(150):
+    for x in range(len(triangles)):
         tri = a3.art3d.Poly3DCollection([triangles[x, :, :3]]/(triangles[x, :,-1]**2)[:, np.newaxis])
         tri.set_color(colors.rgb2hex(np.random.rand(3)))
         tri.set_edgecolor('k')
         ax.add_collection3d(tri)
     plt.show()
 
-plot_3d_projection(np.array(vertices))
+dodecaplex_4d_verts = gen_dodecaplex_vertices()
+tetraplex_4d_verts = gen_tetraplex_vertices()
+
+def yield_dodecahedrons_from_dodecaplex():
+    for ov in tetraplex_4d_verts:
+        yield tuple(v for v in dodecaplex_4d_verts if RADIUS_120CELL-0.01 < np.linalg.norm(np.array(v)-np.array(ov)) < RADIUS_120CELL+0.01)
+
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(projection='3d')
+count = 0
+all_d_verts = yield_dodecahedrons_from_dodecaplex()
+
+color_dict = {}
+origin = np.array(random.choice(tetraplex_4d_verts))
+print("origin: ", origin)
+the_colors = ['black', 'orange', 'purple', 'magenta', 'green', 'red', 'black', 'yellow', 'grey', 'magenta', 'orange', 'black']
+
+vs, os, svs, ots = [],[],[],[]
+
+for dodec_verts in all_d_verts:
+    vertices = np.array(dodec_verts)
+    offset = vertices-origin[np.newaxis,:]
+    #scaled_vertices = (offset)*(PHI**abs(offset[:,-1]-RADIUS_120CELL))[:, np.newaxis] 
+    scaled_vertices = vertices
+    outter_shell =  max([np.linalg.norm(x) for x in offset/(2**(offset[:,-1]/10))[:, np.newaxis] ])
+    vs.append(vertices)
+    svs.append(scaled_vertices)
+    ots.append(outter_shell)
+
+print(set(list(ots)))
+minimum_dist = min(list(ots))
+average_dist = sum(set(list(ots)))/len(set(list(ots)))
+
+for vertices, scaled_vertices, outter_shell in zip(vs, svs, ots):
+    
+    #if outter_shell > average_dist: continue
+    
+    color_new = color_dict.get(outter_shell, the_colors[len(color_dict.keys())%len(the_colors)])
+    color_dict[outter_shell] = color_new
+    
+    ax.scatter3D(scaled_vertices[:, 0], scaled_vertices[:, 1], scaled_vertices[:, 2], s=0.1)
+    for ov,osv in zip(vertices, scaled_vertices):
+        neighbors = np.array([sv-osv for v, sv in zip(vertices, scaled_vertices) if ((EDGE_120CELL-0.01) < np.linalg.norm(np.array(ov)-np.array(v)) < (EDGE_120CELL+0.01))]) 
+        plt.quiver([osv[0] for x in range(len(neighbors))], 
+                   [osv[1] for y in range(len(neighbors))], 
+                   [osv[2] for z in range(len(neighbors))], neighbors[:,0], neighbors[:,1], neighbors[:,2],
+                   color=[color_new for c in range(len(neighbors))], alpha=(1./(outter_shell**3)) if outter_shell!=minimum_dist else 1)
+    count += 1
+
+plt.title(f'3D Projection of vertices with {count} primitives')
+plt.show()
+ 
+#plot_2d_projection(dodecaplex_4d_verts)
+#plot_3d_projection(tetraplex_4d_verts, EDGE=EDGE_600CELL)
+#plot_triangles(dodecaplex_4d_verts)
+
