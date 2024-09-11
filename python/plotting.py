da2590a9c647795700f1f5ce7f60075c30ce1812
Owen Thompson <oxt3479@rit.edu>
2024-09-10 20:19:35 -0700

    Working neighbor transform implementation

--- python/plotting.py
+++ python/plotting.py
@@ -1,10 +1,13 @@
 import pprint
 import random
+import matplotlib.animation
 import numpy as np
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d as a3
 import matplotlib.colors as colors
+import matplotlib
 from fourdsolids import *
+from scipy.spatial import ConvexHull
 
 def plot_2d_projection(vertices):
     vertices = np.array(vertices)
@@ -42,40 +45,51 @@ def plot_3d_projection(vertices, EDGE=EDGE_120CELL, scale=3):
     plt.title(f'3D Projection of vertices')
     plt.show()
 
-def plot_triangles(vertices, EDGE=EDGE_120CELL):
+def animate_projection(vertices_groups, EDGE=EDGE_120CELL, scale=3):
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(projection='3d')        
+    ax.scatter3D(vertices_groups[0][:, 0], vertices_groups[0][:, 1], vertices_groups[0][:, 2], s=0.1)
+    quiver = plt.quiver([])
 
-    neighborhoods = {}
+    def update_graph(idx):
+        vertices = np.array(vertices_groups[idx])
+        
+        for ov,osv in zip(vertices, vertices):
+            neighbors = np.array([sv-osv for v, sv in zip(vertices, vertices) if ((EDGE-0.01) < np.linalg.norm(np.array(ov)-np.array(v)) < (EDGE+0.01))]) 
+            color_new = random.choice(['red', 'green', 'blue', 'yellow', 'purple', 'cyan'])
+            if len(neighbors) < 3: 
+                continue
+            plt.quiver([osv[0] for x in range(len(neighbors))], 
+                    [osv[1] for y in range(len(neighbors))], 
+                    [osv[2] for z in range(len(neighbors))], neighbors[:,0], neighbors[:,1], neighbors[:,2],
+                    color=[color_new for c in range(len(neighbors))], alpha=0.4)
 
-    for ov in vertices:    
-        neighborhood = [v for v in vertices if EDGE-0.01 < np.linalg.norm(np.array(v)-np.array(ov)) < EDGE+0.01]
-        neighborhoods[ov] = neighborhood
+    ax.set_xlim(-scale, scale)
+    ax.set_ylim(-scale, scale)
+    ax.set_zlim(-scale, scale)
+    ani = matplotlib.animation.FuncAnimation(fig, update_graph, 2, interval=10, blit=False)
 
-    triangles = []
-    blacklist= []
-    items = list(neighborhoods.items())
-    random.shuffle(items)
+    plt.show()
 
-    for origin, neighbors in items:
-        #if origin in blacklist: continue
-
-        for a,b in ((0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)):
-            triangles.append([origin, neighbors[a], neighbors[b]])
-        for neighbor in neighbors:
-            blacklist.append(neighbor)
-    triangles = np.array(triangles)
-
-
+def plot_3d_hulls(hulls, scale = 3):
     fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    color_params = ['y-', 'b-']
+    for i, pts in enumerate(hulls):
+        hull = ConvexHull(pts[:,:3])
+        #ax.plot(pts.T[0], pts.T[1], pts.T[2], 'ko')
+        for s in hull.simplices:
+            s = np.append(s, s[0])
+            ax.plot(pts[s, 0], pts[s, 1], pts[s, 2], color_params[i%len(color_params)])
+    
+    for start, end in zip(hulls[0], hulls[1]):
+        plt.quiver(start[0], start[1], start[2], 
+                   end[0]-start[0], end[1]-start[1], end[2]-start[2],
+                   color='red')
 
-    ax = a3.Axes3D(fig)
-    b = 2
-    ax.set(xlim=(-b,b), ylim=(-b,b), zlim=(-b,b))
-    fig.add_axes(ax)
-    for x in range(len(triangles)):
-        tri = a3.art3d.Poly3DCollection([triangles[x, :, :3]]/(triangles[x, :,-1]**2)[:, np.newaxis])
-        tri.set_color(colors.rgb2hex(np.random.rand(3)))
-        tri.set_edgecolor('k')
-        ax.add_collection3d(tri)
+    ax.set_xlim(-scale, scale)
+    ax.set_ylim(-scale, scale)
+    ax.set_zlim(-scale, scale)
     plt.show()
 
 def plot_isolated_cells(dodecaplex_4d_verts, tetraplex_4d_verts, transformation=None):
@@ -168,81 +182,41 @@ def plot_translated_neighbor_maps(t4vs):
             print(f"origin: {o_idx}, neighbor: {n_idx}")
             plot_3d_projection(fPoints, scale = 0.66)
 
-
-if __name__ == "__main__":
-    d4vs, t4vs = gen_dodecaplex_vertices(), gen_tetraplex_vertices()
-    nmap = get_neighbor_map(t4vs)
+def solve_primary_neighbor_transforms(d4vs, t4vs):
     set20s = list(yield_dodecahedrons_from_dodecaplex(d4vs, t4vs))
 
-    """     fig = plt.figure(figsize=(8, 8))
-    ax = fig.add_subplot(projection='3d') """
-
-    colorlist = ['red', 'green', 'blue']
-    cell_idx = 0
-
-    def add_dodec(points):
-        for ov in points:            
-            quiverArrows = np.array([v-ov for v in points if isclose(get_seperation(ov, v), EDGE_120CELL)])
-            if quiverArrows.size > 0:
-                plt.quiver([ov[0] for x in range(len(quiverArrows))], 
-                        [ov[1] for y in range(len(quiverArrows))], 
-                        [ov[2] for z in range(len(quiverArrows))], quiverArrows[:,0], quiverArrows[:,1], quiverArrows[:,2],
-                        color=colorlist[(cell_idx)%len(colorlist)], alpha=1.0)        
-
-
     org_20_set = set20s[0]
-    all_vert_lut = np.zeros((600), np.uint8)
-    rotation_lut = {}
-    colors_ = ['red', 'green', 'blue']
-    for adj_idx in nmap[0]:
-        print(f'Neighbor {adj_idx}')
+    
+    core_transforms = {}
+    
+    for adj_idx in get_neighbors(t4vs[0], t4vs):
         adj_20_set  = set20s[adj_idx]
         adj_cent    = t4vs[adj_idx]
-        adj_rot     = mat_180_around(adj_cent[:3])
-        adj_wall    = adj_20_set & org_20_set
 
-        adj_wall_list = list(adj_wall)
-        clockwise_wall = adj_wall_list.copy()
-        mean_wall = np.mean(np.array(adj_wall_list), axis=0)
-        """ 
-        clockwise_wall.sort(key=lambda x: 
-                            np.inner(np.array(x)-mean_wall,
-                            np.array(adj_wall_list[0])-mean_wall)
-                            )
-        for start, end in zip(clockwise_wall, clockwise_wall[1:]+[clockwise_wall[0]]):
-            plt.quiver(start[0]+adj_cent[0],start[1]+adj_cent[1],start[2]+adj_cent[2], end[0]-start[0],end[1]-start[1],end[2]-start[2], color=colors_[adj_idx%len(colors_)])
-         """
         rot_axis            = adj_cent[:3]
         rot_mat             = mat_180_around(rot_axis)
         adj_20_arr          = np.array(list(adj_20_set))
         dst_20_arr          = np.array(list(org_20_set))
-        org_20_arr          = np.copy(dst_20_arr)
-        dst_20_arr[:,:3]    = np.matmul(rot_mat, dst_20_arr[:,:3].T).T
+        dst_20_arr[:,:3]    = dst_20_arr[:,:3]@rot_mat
 
         idx_map = np.zeros((20), np.uint)
         for in_adj_idx, adj_point in enumerate(adj_20_arr):
             for dst_idx, dst_point in enumerate(dst_20_arr):
                 if np.all(np.linalg.norm(np.cross(np.array(rot_axis), np.array(adj_point-dst_point)[:3])) < 0.01):
                     idx_map[in_adj_idx] = dst_idx
-        
-        print(adj_20_arr.shape)
-        print(dst_20_arr.shape)
-
         adj_20_arr_old = np.copy(adj_20_arr)
-
         for t,f in enumerate(idx_map):
             adj_20_arr[f, :] = adj_20_arr_old[t, :]
 
         fourdee = np.linalg.lstsq(adj_20_arr, dst_20_arr)
-        print(fourdee)
-        #a @ x = b
-
-        plot_3d_projection(np.concatenate((adj_20_arr@fourdee[0],adj_20_arr)))
+        translated = adj_20_arr@fourdee[0]
         
-        
+        #translated[:,:3] =translated[:,:3]@np.linalg.inv(rot_mat)
+        plot_3d_hulls([adj_20_arr, translated])
+        core_transforms[rot_axis] = tuple(rot_mat, fourdee)
 
-    """ ax.set_xlim(-3, 3)
-    ax.set_ylim(-3, 3)
-    ax.set_zlim(-3, 3) """
-
-    # plt.show()
+if __name__ == "__main__":   
+    d4vs, t4vs = gen_dodecaplex_vertices(), gen_tetraplex_vertices()
+    solve_primary_neighbor_transforms(d4vs, t4vs)
+    #nmap = get_neighbor_map(t4vs)
+    #neighbor_arr = np.array([t4vs[ni] for ni in nmap[x]])
